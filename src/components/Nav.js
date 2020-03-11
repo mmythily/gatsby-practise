e37c7bafaf4214588d5808cfd6ccd7710a47cb64
mmythily <mythily.mudunuru@gmail.com>
2020-03-10 23:46:13 -0400

    view posts

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -16,15 +16,10 @@ const Menu = () => {
           justifyContent: 'space-evenly',
         }}
       >
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/services">Services</Link>
-        </li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/blog">Blog</Link></li>
       </ul>
     </div>
   )
---
+++ src/pages/blog.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+export default function blog({data}) {
+  return (
+    <Layout>
+      <SEO title='Blog' description='blog posts' />
+      <h2>Latest Posts</h2>
+      {data.allMarkdownRemark.edges.map(post => (
+        <div key={post.node.id}>
+          <Link to={post.node.frontmatter.path}><h2>{post.node.frontmatter.title}</h2></Link>
+          <p>Posted by {post.node.frontmatter.author} on {post.node.frontmatter.date}</p>
+          
+        </div>
+      ))}
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+    query BlogIndexQuery{
+      allMarkdownRemark{
+        edges{
+          node{
+            id
+            frontmatter{
+              path
+              title
+              date
+              author
+            }
+            excerpt
+          }
+        }
+      }
+    }
+`
